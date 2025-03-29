4d3155cd5f82f7bc1fcd585f11b5c8883077f501
Anirudh-020505 <anirudhpanigrahy7@gmail.com>
2025-03-29 22:45:29 +0530

    Update UserInput.jsx

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const UserInput = ({ userInput, setUserInput }) => {
+const UserInput = ({ userInput, setUserInput, disabled }) => {
   return (
     <input
       type="text"
       value={userInput}
       onChange={(e) => setUserInput(e.target.value)}  // Update input value as the user types
       placeholder="Enter the code"
+      disabled={disabled}  // Disable input based on the 'disabled' prop
     />
   );
 };
